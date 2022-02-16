e133edb01ee3995807b7a384c9d33575b34b3eb2
FernandoDavid <prepa.2001@gmail.com>
2022-02-15 23:30:10 -0600

    Express basic configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-const express = require("express");
-//Body parser
-const bodyParser = require("body-parser");
+import express from 'express';
+import config from './config.js'
 const app = express();
-const PORT = 3000;
 
-//Listening event
-app.listen(PORT, () => {
-	console.log("[DEBUG] - LISTENING ON PORT " + PORT);
+app.get('/', ()=>{
+    res.send("Default route, acces to users/, posts/ or comments/ routes.");
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-
-//Routes
-require("./controllers/UserController")(app);
-require("./controllers/PostController")(app);
+app.listen(config.PORT, ()=>{
+    console.log("Server running on port "+config.PORT);
+});
