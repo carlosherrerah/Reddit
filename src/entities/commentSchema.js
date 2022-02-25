4a0cf5bee8b742f986514ac1c0535ead3fdc779a
FernandoDavid <prepa.2001@gmail.com>
2022-02-25 00:14:31 -0600

    Deleted old version

--- src/entities/commentSchema.js
+++
@@ -1,9 +0,0 @@
-class commentSchema {
-    constructor(post,author,content){//Function with which we create the 'table' row
-        this.post = post,
-        this.author = author,
-        this.content = content
-    }
-}
-
-module.exports = commentSchema;
